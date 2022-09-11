9111d933d27805ecad0e8100c7183ae14a8c133d
Chandra Sekhar Reddy Muthumula <chandu.muthumula@gmail.com>
2022-09-11 12:58:38 +0530

    Added ReverseVowels

---
+++ Problems/ReverseVowels/ReverseVowels.java
@@ -0,0 +1,49 @@
+class ReverseVowels {
+	public static void main (String[] args) {
+		System.out.println("Original String : 'abcde' -> After  reversing vowels : '" + reverseVowels("abcde") + "'");
+		System.out.println("Original String : 'qwerty' -> After  reversing vowels : '" + reverseVowels("qwerty") + "'");
+		System.out.println("Original String : 'ace' -> After  reversing vowels : '" + reverseVowels("ace") + "'");
+		System.out.println("Original String : 'qazxswedc' -> After  reversing vowels : '" + reverseVowels("qazxswedc") + "'");
+		System.out.println("Original String : 'aeiou' -> After  reversing vowels : '" + reverseVowels("aeiou") + "'");
+		System.out.println("Original String : 'aaeeiioouu' -> After  reversing vowels : '" + reverseVowels("aaeeiioouu") + "'");
+		System.out.println("Original String : 'qwrtyp' -> After  reversing vowels : '" + reverseVowels("qwrtyp") + "'");
+		System.out.println("Original String : 'AEIOUaeiou' -> After  reversing vowels : '" + reverseVowels("AEIOUaeiou") + "'");
+		System.out.println("Original String : 'a' -> After  reversing vowels : '" + reverseVowels("a") + "'");
+		
+	}
+    public static String reverseVowels(String s) {
+        char[] ch = s.toCharArray();
+        int start = 0;
+        int end = ch.length - 1;
+        if (1 <= s.length() && s.length() <= 300000 ) {
+            while (start < end) {
+                boolean startFlag = false;
+                boolean endFlag = false;
+                if( !isVowel(ch[start]) ) {
+                    start++;
+                    continue;
+                }
+                if (!isVowel(ch[end])) {
+                   end--;
+                    continue;
+                }
+                if (ch[end] != ch[start]) {
+                    char temp = ch[end];
+                    ch[end] = ch[start];
+                    ch[start] = temp;
+                    start++;end--;
+                } else if (ch[end] == ch[start]) { start++;end--;}
+            }
+        }
+             
+
+        return String.copyValueOf(ch); 
+    }
+    
+    public static boolean isVowel (char c) {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' ) {
+          return true;   
+        } 
+        return false;
+    }
+}
