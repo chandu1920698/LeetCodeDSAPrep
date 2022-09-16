21c29080b56ba01de1cf8aaf70408f63bcd962db
Chandra Sekhar Reddy Muthumula <chandu.muthumula@gmail.com>
2022-09-17 00:41:06 +0530

    Changed into Subset money problem

--- Problems/SubsetsOfArray/SubsetsOfArray.java
+++ Problems/SubsetsOfArray/SubsetsOfArray.java
@@ -6,8 +6,9 @@ Author      : Chandra Sekhar Reddy Muthumula
 Website Link: https://www.youtube.com/watch?v=0AAjJvlq5Is&list=PLfQN-EvRGF39Vz4UO18BtA1ocnUhGkvk5&index=16&ab_channel=CodesYourMind
 Modification Log: 
 Date					Name                                            Description
-Sep 14, 2022			Chandra Sekhar Reddy Muthumula					Added Class SubsetsOfArray 
-Sep 14, 2022			Chandra Sekhar Reddy Muthumula					Added solutionMethod 
+Sep 16, 2022			Chandra Sekhar Reddy Muthumula					Added Class SubsetsOfArray 
+Sep 16, 2022			Chandra Sekhar Reddy Muthumula					Added solutionMethod 
+Sep 16, 2022			Chandra Sekhar Reddy Muthumula					Changed ino the Subset Money problem
 --------------------------------------------------------------------------------------------------
 Problem Statement: Find The Unique Two Numbers Using Bit Manipultion
 
@@ -25,29 +26,56 @@ import java.util.*;
 public class SubsetsOfArray {
 	public static void main (String[] args) {
 		// Test Cases
-		solutionMethod(new int [] {1,2,3});
+		System.out.println(solutionMethod(new int [] {1,2,3}, 5));
 		System.out.println("--------------------------------------------------");
-		solutionMethod(new int [] {4,2,3});
+		System.out.println(solutionMethod(new int [] {4,2,3}, 8));
 		System.out.println("--------------------------------------------------");
-		solutionMethod(new int [] {1,5,7,8,4,2,3});
+		System.out.println(solutionMethod(new int [] {1,5,7,4,2,3}, 21));
 		System.out.println("--------------------------------------------------");
 	}
 	
-	public static void solutionMethod (int[] arr) {
+	public static boolean solutionMethod (int[] arr, int money) {
+		System.out.println("money : " + money);
 		if (arr.length <= 0) {
 			System.out.println("Invalid Input");
 		}
 		int total = 1<<arr.length;
-		for (int k=1; k<total; k++) {
+		
+		for (int k=1; k<total-1; k++) {
+			int subsetSum = 0;
 			for (int i=0; i<arr.length; i++) {
+				
 				// System.out.println("Data -> " + (k & (1<<i)));
 				if ( ((k>>i)&1) == 1 ) {
-					System.out.print(arr[i] + " ");
+					subsetSum += arr[i];
+					// System.out.print(arr[i] + " ");
 				}
 			}
-			System.out.println();
+			//System.out.println("subsetSum : " + subsetSum);
+			
+			if (money == subsetSum)  {
+				//System.out.println("money == subsetSum"); 
+				return true;
+			} else {
+				continue;
+			}
+			//System.out.println();
 		}
+		return false;
 		
 	}
 	
 }
+
+/*
+OUTPUT:
+money : 5
+true
+--------------------------------------------------
+money : 8
+false
+--------------------------------------------------
+money : 21
+true
+--------------------------------------------------
+*/
