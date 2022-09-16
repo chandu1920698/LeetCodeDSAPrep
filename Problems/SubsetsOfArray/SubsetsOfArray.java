e1064dc2e85db8a924120add1c0091d1b1304932
Chandra Sekhar Reddy Muthumula <chandu.muthumula@gmail.com>
2022-09-17 00:19:37 +0530

    Added SubsetsOfArray

---
+++ Problems/SubsetsOfArray/SubsetsOfArray.java
@@ -0,0 +1,53 @@
+/*
+Class Name  : SubsetsOfArray
+Description : This class consists of the solution for SubsetsOfArray.
+Date        : Sep 16, 2022
+Author      : Chandra Sekhar Reddy Muthumula
+Website Link: https://www.youtube.com/watch?v=0AAjJvlq5Is&list=PLfQN-EvRGF39Vz4UO18BtA1ocnUhGkvk5&index=16&ab_channel=CodesYourMind
+Modification Log: 
+Date					Name                                            Description
+Sep 14, 2022			Chandra Sekhar Reddy Muthumula					Added Class SubsetsOfArray 
+Sep 14, 2022			Chandra Sekhar Reddy Muthumula					Added solutionMethod 
+--------------------------------------------------------------------------------------------------
+Problem Statement: Find The Unique Two Numbers Using Bit Manipultion
+
+You are given an array of Integers. 
+The special property of the arrray is that exactly two different elemenst occur once, while others occur twice.
+You are required to determine those two elements.
+
+Example :
+Input: nums = [1,2,3,4,5,3,4,5]
+Output: [1,2]
+
+
+*/
+import java.util.*;
+public class SubsetsOfArray {
+	public static void main (String[] args) {
+		// Test Cases
+		solutionMethod(new int [] {1,2,3});
+		System.out.println("--------------------------------------------------");
+		solutionMethod(new int [] {4,2,3});
+		System.out.println("--------------------------------------------------");
+		solutionMethod(new int [] {1,5,7,8,4,2,3});
+		System.out.println("--------------------------------------------------");
+	}
+	
+	public static void solutionMethod (int[] arr) {
+		if (arr.length <= 0) {
+			System.out.println("Invalid Input");
+		}
+		int total = 1<<arr.length;
+		for (int k=1; k<total; k++) {
+			for (int i=0; i<arr.length; i++) {
+				// System.out.println("Data -> " + (k & (1<<i)));
+				if ( ((k>>i)&1) == 1 ) {
+					System.out.print(arr[i] + " ");
+				}
+			}
+			System.out.println();
+		}
+		
+	}
+	
+}
