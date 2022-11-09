bf539f90c7cad4c49c24945e3c87bf070346a360
Chandra Sekhar Reddy Muthumula <chandu.muthumula@gmail.com>
2022-11-10 00:24:44 +0530

    Added PowXN

---
+++ Problems/PowXN/PowXN.java
@@ -0,0 +1,69 @@
+/*
+Class Name  : PowXN
+Description : This class consists of the solution for PowXN.
+Date        : Nov 9, 2022
+Author      : Chandra Sekhar Reddy Muthumula
+Link        : https://leetcode.com/problems/powx-n/description/
+
+Modification Log: 
+Date				Name                                            Description
+Nov 9, 2022			Chandra Sekhar Reddy Muthumula					Added Class PowXN 
+Nov 9, 2022			Chandra Sekhar Reddy Muthumula					Added Class myPow 
+--------------------------------------------------------------------------------------------------
+50. Pow(x, n)
+Medium
+6.1K
+6.6K
+Companies
+Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+ 
+
+Example 1:
+
+Input: x = 2.00000, n = 10
+Output: 1024.00000
+Example 2:
+
+Input: x = 2.10000, n = 3
+Output: 9.26100
+Example 3:
+
+Input: x = 2.00000, n = -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+ 
+
+Constraints:
+
+-100.0 < x < 100.0
+-231 <= n <= 231-1
+n is an integer.
+-104 <= xn <= 104
+-------------------------------------------------------------------------------------------
+*/
+package Problems.PowXN;
+
+public class PowXN {
+    public double myPow(double x, int n) {
+        /*
+         * TC : O(LogN)
+         * SC : O(1)
+        */
+        if (x == 0) return 0;
+        if (n == 0) return 1.0; 
+        double result = 1.0d;
+        if (n < 0) {
+            x = 1/x;
+            n = -1 * (n + 1);
+            result *= x;
+        }
+        
+        if (n % 2 == 0) {
+            result *= myPow(x * x, n / 2); 
+        }  else {
+            result *= (x * myPow(x * x, n / 2));
+        }
+        return result;
+    }
+}
