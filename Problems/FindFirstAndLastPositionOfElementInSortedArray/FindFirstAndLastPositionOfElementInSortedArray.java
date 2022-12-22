4d5c86e826c75618d727f34bd36a082ebcebdf45
Chandra Sekhar Reddy Muthumula <chandu.muthumula@gmail.com>
2022-12-22 22:27:14 +0530

    Added searchRangeBestApproach method

--- Problems/FindFirstAndLastPositionOfElementInSortedArray/FindFirstAndLastPositionOfElementInSortedArray.java
+++ Problems/FindFirstAndLastPositionOfElementInSortedArray/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -1,7 +1,7 @@
 /*
 Class Name  : FindFirstAndLastPositionOfElementInSortedArray
 Description : This class consists of the solution for FindFirstAndLastPositionOfElementInSortedArray.
-Date        : Oct 24, 2022
+Created Date: Oct 24, 2022
 Author      : Chandra Sekhar Reddy Muthumula
 Website Link: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 
@@ -9,6 +9,8 @@ Modification Log:
 Date					Name                                            Description
 Oct 24, 2022			Chandra Sekhar Reddy Muthumula					Added Class FindFirstAndLastPositionOfElementInSortedArray 
 Oct 24, 2022			Chandra Sekhar Reddy Muthumula					Added searchRange 
+Dec 22, 2022			Chandra Sekhar Reddy Muthumula					Added searchRangeBestApproach
+Dec 22, 2022			Chandra Sekhar Reddy Muthumula					Added binarySearch
 --------------------------------------------------------------------------------------------------
 34. Find First and Last Position of Element in Sorted Array
 
@@ -83,5 +85,43 @@ public class FindFirstAndLastPositionOfElementInSortedArray {
             }
         }
         return result;       
+    }
+
+    public int[] searchRangeBestApproach(int[] nums, int target) {
+        int len = nums.length;
+        // This is an edge case where the length of nums is 1 and the target is matching
+        if(len == 1) {
+            if(target == nums[0]) {
+                return new int[] {0, 0};
+            }
+        } 
+        int[] result = new int[2];
+        result[0] = binarySearch(nums, target, true);
+        result[1] = binarySearch(nums, target, false);
+       
+        return result;       
+    }
+
+    public int binarySearch(int[] nums, int target, boolean findStartIndex )  {
+        int ans = -1;
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + ((end - start) >> 1);
+            if(nums[mid] == target) {
+                ans = mid;
+                if (findStartIndex) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return ans;
     }
 }
