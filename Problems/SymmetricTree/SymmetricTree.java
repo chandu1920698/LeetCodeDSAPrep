6a9e8d0c48194309cb093d245b777e0475b2626f
Chandra Sekhar Reddy Muthumula <chandu.muthumula@gmail.com>
2023-01-15 22:24:42 +0530

    Added isSymmetricIterativeApproach method

--- Problems/SymmetricTree/SymmetricTree.java
+++ Problems/SymmetricTree/SymmetricTree.java
@@ -10,6 +10,7 @@ Date				    Name                                            Description
 Jan 15, 2022			Chandra Sekhar Reddy Muthumula					Added Class SymmetricTree
 Jan 15, 2022			Chandra Sekhar Reddy Muthumula					Added isSymmetric
 Jan 15, 2022			Chandra Sekhar Reddy Muthumula					Added isSymmetricHelper
+Jan 15, 2022			Chandra Sekhar Reddy Muthumula					Added isSymmetricIterativeApproach
 --------------------------------------------------------------------------------------------------
 101. Symmetric Tree
 Easy
@@ -42,7 +43,7 @@ Follow up: Could you solve it both recursively and iteratively?
 -------------------------------------------------------------------------------------------
 */
 package Problems.SymmetricTree;
-
+import java.util.*;
 public class SymmetricTree {
     public boolean isSymmetric(TreeNode root) {
         /* 
@@ -64,6 +65,32 @@ public class SymmetricTree {
             return false;
         }
         return (tempRootLeft.val == tempRootRight.val) && isSymmetricHelper(tempRootLeft.left, tempRootRight.right) && isSymmetricHelper(tempRootLeft.right, tempRootRight.left);
+    }
+
+    public boolean isSymmetricIterativeApproach(TreeNode root) {
+        /* 
+         * TC : O(N)
+         * SC : O(1)
+        */
+        if(root == null) return true;
+        TreeNode tempRoot = root;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(tempRoot.left);
+        stack.push(tempRoot.right);
+
+        while(!stack.isEmpty()) {
+            TreeNode n1 = stack.pop(); // right
+            TreeNode n2 = stack.pop(); // left
+
+            if(n1 == null && n2 == null) continue;
+            if(n1 == null || n2 == null || n1.val != n2.val) return false; 
+
+            stack.push(n1.left);
+            stack.push(n2.right);
+            stack.push(n1.right);
+            stack.push(n2.left);
+        }
+        return true;
     }
 }
 class TreeNode {
