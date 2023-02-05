ab68d0a4c165d1a2887cf018bc0930163bde295e
Chandra Sekhar Reddy Muthumula <chandu.muthumula@gmail.com>
2023-02-05 16:14:57 +0530

    Added FairCandySwap

---
+++ Problems/FairCandySwap/FairCandySwap.java
@@ -0,0 +1,111 @@
+/*
+Class Name  : FairCandySwap
+Description : This class consists of the solution for FairCandySwap.
+Created Date: Feb 5, 2022
+Author      : Chandra Sekhar Reddy Muthumula
+Website Link: https://leetcode.com/problems/fair-candy-swap/description/
+Modification Log: 
+Date					Name                                            Description
+Feb 5, 2022			    Chandra Sekhar Reddy Muthumula					Added Class FairCandySwap 
+Feb 5, 2022			    Chandra Sekhar Reddy Muthumula					Added fairCandySwap
+Feb 5, 2022			    Chandra Sekhar Reddy Muthumula					Added binarySearch
+--------------------------------------------------------------------------------------------------
+888. Fair Candy Swap
+Easy
+1.7K
+313
+Companies
+Alice and Bob have a different total number of candies. 
+You are given two integer arrays aliceSizes and bobSizes 
+where aliceSizes[i] is the number of candies of the ith box of candy that Alice has and 
+bobSizes[j] is the number of candies of the jth box of candy that Bob has.
+
+Since they are friends, they would like to exchange one candy box each so that after the exchange,
+they both have the same total amount of candy. 
+The total amount of candy a person has is the sum of the number of candies in each box they have.
+
+Return an integer array answer where answer[0] is the number of candies in the box 
+that Alice must exchange, and answer[1] is the number of candies in the box that Bob must exchange. If there are multiple answers, you may return any one of them. It is guaranteed that at least one answer exists.
+
+ 
+
+Example 1:
+
+Input: aliceSizes = [1,1], bobSizes = [2,2]
+Output: [1,2]
+Example 2:
+
+Input: aliceSizes = [1,2], bobSizes = [2,3]
+Output: [1,2]
+Example 3:
+
+Input: aliceSizes = [2], bobSizes = [1,3]
+Output: [2,3]
+ 
+
+Constraints:
+
+1 <= aliceSizes.length, bobSizes.length <= 104
+1 <= aliceSizes[i], bobSizes[j] <= 105
+Alice and Bob have a different total number of candies.
+There will be at least one valid answer for the given input.
+------------------------------------------------------------------------------------------------------
+*/
+package Problems.FairCandySwap;
+
+import java.util.*;
+
+public class FairCandySwap {
+    public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+        /*
+         * TC : O(N LogN)
+         * SC : O(1)
+         * 
+         * 
+         * Let Sum of choclates by Alice Be Sa and Sum of choclates by Alice Be Sb
+         * Let the choclates from Alice to Bob be A and from Bob to Alice be B
+         *
+         * Sa -A + B = Sb -B + A
+         * 2(A-B) = (Sa-Sb)
+         * A-B = (Sa-Sb)/2
+         * A = B+(Sa-Sb)/2
+         */
+        int aliseLen = aliceSizes.length;
+        int bobLen = bobSizes.length;
+
+        long aliceSum = 0, bobSum = 0;
+        int maxLen = Math.max(aliseLen, bobLen);
+        for (int i = 0; i < maxLen; i++) {
+            if (i < aliseLen) {
+                aliceSum += aliceSizes[i];
+            }
+            if (i < bobLen) {
+                bobSum += bobSizes[i];
+            }
+        }
+        int diff = (int) (aliceSum - bobSum) / 2;
+        Arrays.sort(aliceSizes);
+        for (int num : bobSizes) {
+            if (binarySearch(aliceSizes, num + diff) != -1) {
+                return new int[] { num + diff, num };
+            }
+        }
+        return new int[] {};
+    }
+
+    private int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + ((end - start) >> 1);
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
