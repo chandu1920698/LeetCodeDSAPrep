5030f76350cd06b86ac2436a9430e79a17ddd7f7
Chandra Sekhar Reddy Muthumula <chandu.muthumula@gmail.com>
2023-02-07 01:27:28 +0530

    Added SubstringWithConcatenationOfAllWords

---
+++ Problems/SubstringWithConcatenationOfAllWords/SubstringWithConcatenationOfAllWords.java
@@ -0,0 +1,106 @@
+/*
+Class Name  : SubstringWithConcatenationOfAllWords
+Description : This class consists of the solution for SubstringWithConcatenationOfAllWords.
+Created Date: Feb 7, 2022
+Author      : Chandra Sekhar Reddy Muthumula
+Website Link: https://leetcode.com/problems/substring-with-concatenation-of-all-words/description/
+Modification Log: 
+Date					Name                                            Description
+Feb 7, 2022			    Chandra Sekhar Reddy Muthumula					Added Class SubstringWithConcatenationOfAllWords 
+Feb 7, 2022			    Chandra Sekhar Reddy Muthumula					Added findSubstring
+--------------------------------------------------------------------------------------------------
+30. Substring with Concatenation of All Words
+Hard
+567
+36
+Companies
+You are given a string s and an array of strings words. 
+All the strings of words are of the same length.
+
+A concatenated substring in s is a substring that contains all the strings of any permutation of words concatenated.
+
+For example, if words = ["ab","cd","ef"], then "abcdef", "abefcd", "cdabef", "cdefab", "efabcd", 
+and "efcdab" are all concatenated strings. "acdbef" is not a concatenated substring because it is not the concatenation of any permutation of words.
+Return the starting indices of all the concatenated substrings in s. 
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: s = "barfoothefoobarman", words = ["foo","bar"]
+Output: [0,9]
+Explanation: Since words.length == 2 and words[i].length == 3, the concatenated substring has to be of length 6.
+The substring starting at 0 is "barfoo". It is the concatenation of ["bar","foo"] which is a permutation of words.
+The substring starting at 9 is "foobar". It is the concatenation of ["foo","bar"] which is a permutation of words.
+The output order does not matter. Returning [9,0] is fine too.
+Example 2:
+
+Input: s = "wordgoodgoodgoodbestword", words = ["word","good","best","word"]
+Output: []
+Explanation: Since words.length == 4 and words[i].length == 4, the concatenated substring has to be of length 16.
+There is no substring of length 16 is s that is equal to the concatenation of any permutation of words.
+We return an empty array.
+Example 3:
+
+Input: s = "barfoofoobarthefoobarman", words = ["bar","foo","the"]
+Output: [6,9,12]
+Explanation: Since words.length == 3 and words[i].length == 3, the concatenated substring has to be of length 9.
+The substring starting at 6 is "foobarthe". It is the concatenation of ["foo","bar","the"] which is a permutation of words.
+The substring starting at 9 is "barthefoo". It is the concatenation of ["bar","the","foo"] which is a permutation of words.
+The substring starting at 12 is "thefoobar". It is the concatenation of ["the","foo","bar"] which is a permutation of words.
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+1 <= words.length <= 5000
+1 <= words[i].length <= 30
+s and words[i] consist of lowercase English letters.
+------------------------------------------------------------------------------------------------------
+*/
+
+package Problems.SubstringWithConcatenationOfAllWords;
+import java.util.*;
+public class SubstringWithConcatenationOfAllWords {
+    public List<Integer> findSubstring(String s, String[] words) {
+        /* 
+         * TC : O(sLen * noOfWords * windowSize)
+         * SC : O(noOfWords)
+        */
+        List<Integer> list = new ArrayList<>();
+        int noOfWords = words.length;
+        if(noOfWords == 0) {
+            return list;
+        }
+        int sLen = s.length();
+        int windowLength = words[0].length();
+
+        HashMap<String, Integer> wordFreq = new HashMap<>();
+        for(String word: words) {
+            wordFreq.put(word, wordFreq.getOrDefault(word, 0) + 1);
+        }
+
+        for(int i = 0; i < sLen - noOfWords * windowLength + 1; i++) {
+            int j = 0;
+            HashMap<String, Integer> seenFreq = new HashMap<>();
+            while(j < noOfWords) {
+                final String windowString = s.substring(i + j * windowLength, i + (j + 1) * windowLength);
+                if(wordFreq.containsKey(windowString)) {
+                    seenFreq.put(windowString, seenFreq.getOrDefault(windowString, 0) + 1);
+                    if(seenFreq.get(windowString) > wordFreq.getOrDefault(windowString, 0)) {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+                j++;
+            }
+
+            if(j == noOfWords) {
+                list.add(i);
+            }
+        }
+        return list;
+    }
+}
