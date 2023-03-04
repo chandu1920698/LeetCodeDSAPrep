317eecbc6f801b04357976e10c4a4d2f45d05208
Chandra Sekhar Reddy Muthumula <chandu.muthumula@gmail.com>
2023-03-05 00:30:49 +0530

    Added InsertInterval

---
+++ Problems/InsertInterval/InsertInterval.java
@@ -0,0 +1,79 @@
+/*
+Class Name  : InsertInterval
+Description : This class consists of the solution for InsertInterval.
+Created Date: Mar 5, 2022
+Author      : Chandra Sekhar Reddy Muthumula
+Website Link: https://leetcode.com/problems/insert-interval/description/
+Modification Log: 
+Date					    Name                                            Description
+Mar  5, 2022			    Chandra Sekhar Reddy Muthumula					Added Class InsertInterval
+Mar  5, 2022			    Chandra Sekhar Reddy Muthumula					Added insert
+--------------------------------------------------------------------------------------------------
+57. Insert Interval
+Medium
+8K
+555
+Companies
+You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+
+Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+Return intervals after the insertion.
+
+ 
+
+Example 1:
+
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+Example 2:
+
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+Output: [[1,2],[3,10],[12,16]]
+Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+ 
+
+Constraints:
+
+0 <= intervals.length <= 104
+intervals[i].length == 2
+0 <= starti <= endi <= 105
+intervals is sorted by starti in ascending order.
+newInterval.length == 2
+0 <= start <= end <= 105
+------------------------------------------------------------------------------------------------------
+*/
+package Problems.InsertInterval;
+
+import java.util.*;
+
+public class InsertInterval {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        /* 
+         * TC : O(N)
+         * SC : O(N)
+        */
+
+        List<int[]> list = new ArrayList<>();
+
+        for (int[] interval : intervals) {
+            if (interval[1] < newInterval[0]) {
+                list.add(interval);
+            } else if (interval[0] > newInterval[1]) {
+                list.add(newInterval);
+                newInterval = interval;
+            } else {
+                newInterval[0] = Math.min(interval[0], newInterval[0]);
+                newInterval[1] = Math.max(interval[1], newInterval[1]);
+            }
+        }
+        list.add(newInterval);
+
+        int[][] finalIntervals = new int[list.size()][];
+        int index = 0;
+        for (int[] interval : list) {
+            finalIntervals[index++] = interval;
+        }
+        return finalIntervals;
+    }
+}
