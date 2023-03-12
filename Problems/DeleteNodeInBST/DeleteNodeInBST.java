d8900093671a4a82106d01a955d04898dc97819d
Chandra Sekhar Reddy Muthumula <chandu.muthumula@gmail.com>
2023-03-13 02:17:57 +0530

    Updaed DeleteNodeInBST

--- Problems/DeleteNodeInBST/DeleteNodeInBST.java
+++ Problems/DeleteNodeInBST/DeleteNodeInBST.java
@@ -11,36 +11,49 @@ Mar 12, 2022			    Chandra Sekhar Reddy Muthumula					Added deleteNode
 Mar 12, 2022			    Chandra Sekhar Reddy Muthumula					Added helper 
 Mar 12, 2022			    Chandra Sekhar Reddy Muthumula					Added getRightMostNode 
 --------------------------------------------------------------------------------------------------
-617. Merge Two Binary Trees
-Easy
-7.9K
-275
+450. Delete Node in a BST
+Medium
+7.1K
+179
 Companies
-You are given two binary trees root1 and root2.
+Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
 
-Imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not. You need to merge the two trees into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of the new tree.
+Basically, the deletion can be divided into two stages:
 
-Return the merged tree.
-
-Note: The merging process must start from the root nodes of both trees.
-
+Search for a node to remove.
+If the node is found, delete the node.
  
 
 Example 1:
 
 
-Input: root1 = [1,3,2,5], root2 = [2,1,3,null,4,null,7]
-Output: [3,4,5,5,4,null,7]
+Input: root = [5,3,6,2,4,null,7], key = 3
+Output: [5,4,6,2,null,null,7]
+Explanation: Given key to delete is 3. So we find the node with value 3 and delete it.
+One valid answer is [5,4,6,2,null,null,7], shown in the above BST.
+Please notice that another valid answer is [5,2,6,null,4,null,7] and it's also accepted.
+
 Example 2:
 
-Input: root1 = [1], root2 = [1,2]
-Output: [2,2]
+Input: root = [5,3,6,2,4,null,7], key = 0
+Output: [5,3,6,2,4,null,7]
+Explanation: The tree does not contain a node with value = 0.
+Example 3:
+
+Input: root = [], key = 0
+Output: []
  
 
 Constraints:
 
-The number of nodes in both trees is in the range [0, 2000].
--104 <= Node.val <= 104
+The number of nodes in the tree is in the range [0, 104].
+-105 <= Node.val <= 105
+Each node has a unique value.
+root is a valid binary search tree.
+-105 <= key <= 105
+ 
+
+Follow up: Could you solve it with time complexity O(height of tree)?
 ------------------------------------------------------------------------------------------------------
 */
 package Problems.DeleteNodeInBST;
